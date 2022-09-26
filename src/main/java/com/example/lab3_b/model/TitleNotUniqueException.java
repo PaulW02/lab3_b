1334deec644b0f386bacc23aa500cc12a92332ba
sinemuzunel <102513612+sinemuzunel@users.noreply.github.com>
2022-09-26 15:09:56 +0200

    TitleNotUniqueException funktioner

---
+++ src/main/java/com/example/lab3_b/model/TitleNotUniqueException.java
@@ -0,0 +1,13 @@
+package com.example.lab3_b.model;
+
+public class TitleNotUniqueException extends RuntimeException{
+
+    public TitleNotUniqueException(){
+
+    }
+
+    public TitleNotUniqueException(String mag){
+
+    }
+
+}
